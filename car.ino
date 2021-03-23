aae482e86cc2f73b539003cba1a1c27a2078e572
Abdelrahman Elsayed Mohamed <60861577+abdelrahman99999@users.noreply.github.com>
2021-03-23 15:03:15 +0200

    Create car.ino

---
+++ car.ino
@@ -0,0 +1,207 @@
+
+//declaration of motors
+
+int right_motor_speed = 3;
+int right_motor_backward = 5;
+int right_motor_forward = 4;
+
+int left_motor_speed = 6;
+int left_motor_backward = 8;
+int left_motor_forward = 7;
+
+int Speed = 255 ;         //car speed 
+int low_speed = Speed/2;     //(for rotaions)
+
+// Voids:
+
+void forward()
+{
+  //move both motors FORWARD
+        analogWrite (right_motor_speed, Speed);
+        digitalWrite(right_motor_forward, HIGH);
+        digitalWrite(right_motor_backward, LOW);
+               
+        analogWrite (left_motor_speed,   Speed);
+        digitalWrite(left_motor_forward,  HIGH);        
+        digitalWrite(left_motor_backward,  LOW);
+}
+
+void backward()
+{
+  //move both motors BACKWARD
+        analogWrite (right_motor_speed,  Speed);
+        digitalWrite(right_motor_forward,  LOW);
+        digitalWrite(right_motor_backward,HIGH);
+               
+        analogWrite (left_motor_speed,   Speed);
+        digitalWrite(left_motor_forward,   LOW);        
+        digitalWrite(left_motor_backward, HIGH);
+}
+
+void right()
+{
+  //move car RIGHT
+        analogWrite (right_motor_speed,   0   );
+        digitalWrite(right_motor_forward,  LOW);
+        digitalWrite(right_motor_backward, LOW);
+               
+        analogWrite (left_motor_speed,   Speed);
+        digitalWrite(left_motor_forward,  HIGH);        
+        digitalWrite(left_motor_backward,  LOW);
+}
+
+void left ()
+{
+   //move car LEFT
+        analogWrite (right_motor_speed,  Speed);
+        digitalWrite(right_motor_forward, HIGH);
+        digitalWrite(right_motor_backward, LOW);
+               
+        analogWrite (left_motor_speed,    0   );
+        digitalWrite(left_motor_forward,   LOW);        
+        digitalWrite(left_motor_backward,  LOW);
+}
+
+void forward_left()
+{
+     //move the right motor with the full speed
+
+        analogWrite (right_motor_speed, Speed);
+        digitalWrite(right_motor_forward, HIGH);
+        digitalWrite(right_motor_backward, LOW);
+
+      //move the right motor with a lower speed
+
+        analogWrite(left_motor_speed,low_speed);
+        digitalWrite(left_motor_forward,  HIGH);        
+        digitalWrite(left_motor_backward,  LOW);
+}
+
+
+void forward_right ()
+{
+        //move the right motor with a low speed
+
+      analogWrite (right_motor_speed,low_speed);
+      digitalWrite(right_motor_forward,   HIGH);
+      digitalWrite(right_motor_backward,   LOW);
+
+     //move the right motor with the full speed
+     
+      analogWrite(left_motor_speed,      Speed);
+      digitalWrite(left_motor_forward,    HIGH); 
+      digitalWrite(left_motor_backward,    LOW);
+}
+
+void backward_left()
+{
+     //move the right motor with the full speed
+
+        analogWrite (right_motor_speed,  Speed);
+        digitalWrite(right_motor_forward,  LOW);
+        digitalWrite(right_motor_backward,HIGH);
+
+      //move the right motor with a lower speed
+
+        analogWrite(left_motor_speed,low_speed);
+        digitalWrite(left_motor_forward,   LOW);        
+        digitalWrite(left_motor_backward, HIGH);
+}
+
+void backward_right()
+{
+       //move the right motor with the full speed
+
+        analogWrite (right_motor_speed,low_speed);
+        digitalWrite(right_motor_forward,    LOW);
+        digitalWrite(right_motor_backward,  HIGH);
+
+        //move the right motor with a lower speed
+
+        analogWrite(left_motor_speed,      Speed);
+        digitalWrite(left_motor_forward,     LOW);        
+        digitalWrite(left_motor_backward,   HIGH);
+}
+
+void Stop()
+{
+        analogWrite (right_motor_speed,     0   ); 
+        analogWrite(left_motor_speed,     0     );
+        digitalWrite(11
+        ,0);
+} 
+
+void setup() {
+
+  Serial.begin(9600);
+
+  pinMode(11, OUTPUT);
+  pinMode(right_motor_speed, OUTPUT);
+  pinMode(right_motor_backward, OUTPUT);
+  pinMode(right_motor_forward, OUTPUT);
+
+  pinMode(left_motor_speed, OUTPUT);
+  pinMode(left_motor_backward, OUTPUT);
+  pinMode(left_motor_forward, OUTPUT);
+
+}
+
+void loop() {
+   
+  if(Serial.available() > 0)
+  {
+    char input;
+    input = Serial.read();
+    Serial.println(input);
+    switch (input)
+    {
+      case 'Y' :
+      digitalWrite(11,1);
+      break;
+      case 'F': //FORWARD
+        forward();
+          break;
+          
+      case 'B': //REVERSE
+        backward();
+          break;
+
+      case 'R': //Right
+        right();
+          break;
+
+      case 'L': //LEFT
+        left();
+          break;
+
+      case 'G': //FORWARD LEFT
+        forward_left();
+          break;
+        
+      case 'I': //FORWARD RIGHT
+        forward_right();
+          break;
+        
+      case 'H': //BACKWARD LEFT
+        backward_left();
+         break;
+        
+      case 'J':
+        backward_right();
+          break;
+
+
+
+          case 'S':
+          Stop();
+          break;
+                
+      default: //If bluetooth module receives any value not listed above, both motors turn off
+        Stop();
+        break;
+
+ 
+    }
+  }
+ 
+}
